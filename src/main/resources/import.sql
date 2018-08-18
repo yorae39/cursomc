745300a29173ad625bbeb230a25416dc075286f9
Luiz Paulo <lpaureliano74@gmail.com>
2018-08-18 17:10:59 -0300

    Estado e Cidade

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,5 +12,13 @@ insert into Produto_Categoria (produto_id, categoria_id) values (2,1);
 insert into Produto_Categoria (produto_id, categoria_id) values (3,1);
 insert into Produto_Categoria (produto_id, categoria_id) values (2,2);
 -- -------------------------------------------------------------
+insert into Estado (id, nome) values (null,'Rio de Janeiro');
+insert into Estado (id, nome) values (null,'São Paulo');
+-- -------------------------------------------------------------
+insert into Cidade (id, nome, estado_id) values (null,'São Gonçalo', 1);
+insert into Cidade (id, nome, estado_id) values (null,'Rio de Janeiro', 1);
+insert into Cidade (id, nome, estado_id) values (null,'São Paulo', 2);
+insert into Cidade (id, nome, estado_id) values (null,'Campinas', 2);
+-- ----------------------------------------------------------------
 
 
